ef15485775eaabc66ec80ae7faed32a73d6f3c25
PeppeTll <peppetll@gmail.com>
2023-05-11 17:37:24 +0200

    esercizio 11-05-2023

---
+++ 11-05-2023/esercizio_1_Avantato/app.js
@@ -0,0 +1,120 @@
+const createEL = (el, clas = '', text = '') => {
+  let element = cE(el);
+  element.classList.add(clas);
+  element.innerHTML = text;
+  body.appendChild(element);
+}
+
+const showElement = () => {
+  listData.forEach(item => {
+    const li_El = cE('li');
+    li_El.className = 'li_el';
+    li_El.dataset.value = item.id
+    li_El.textContent = item.title;
+    item.check ? li_El.classList.add('check') : '';
+    ul_El.prepend(li_El);
+  })
+}
+
+const itemCheck = () => {
+  li_items.forEach(item => {
+    item.addEventListener('click', e => {
+      listData.forEach(obj => {
+        if (obj.id === parseInt(e.target.dataset.value)) {
+          obj.check = !obj.check
+          item.classList.toggle('check')
+        }
+      })
+    })
+  })
+}
+
+const qS = el => document.querySelector(el);
+const cE = el => document.createElement(el);
+const qSA = el => document.querySelectorAll(el);
+const body = qS('body');
+
+createEL('input', 'input_el');
+createEL('ul', 'ul_el');
+createEL('button', 'btn_del', '<i class="fa-regular fa-trash-can"></i>');
+
+const input_El = qS('.input_el');
+const ul_El = qS('.ul_el');
+const btnDel = qS('.btn_del');
+const listData = [
+  {
+    id: 1,
+    title: 'lorem ipsum dolor sit amet',
+    check: true,
+  },
+  {
+    id: 2,
+    title: 'lorem ipsum dolor sit amet',
+    check: true,
+  },
+  {
+    id: 3,
+    title: 'lorem ipsum dolor sit amet',
+    check: false,
+  },
+  {
+    id: 4,
+    title: 'lorem ipsum dolor sit amet',
+    check: false,
+  },
+  {
+    id: 5,
+    title: 'lorem ipsum dolor sit amet',
+    check: false,
+  },
+  {
+    id: 6,
+    title: 'lorem ipsum dolor sit amet',
+    check: false,
+  },
+];
+
+showElement();
+
+const li_items = qSA('li');
+
+
+input_El.addEventListener('change', (e) => {
+  const inputObj = {
+    id: Math.floor(Math.random() * 100),
+    title: e.target.value,
+    check: false
+  }
+  ul_El.textContent = '';
+  listData.push(inputObj);
+  e.target.value = ''
+  showElement();
+});
+
+li_items.forEach(item => {
+  item.addEventListener('click', e => {
+    listData.forEach(obj => {
+      if (obj.id === parseInt(e.target.dataset.value)) {
+        obj.check = !obj.check
+        item.classList.toggle('check')
+      }
+    })
+  })
+})
+
+btnDel.addEventListener('click', () => {
+  listData.forEach((item, i) => {
+    if (item.check) {
+      listData.splice(i, 1);
+    }
+  })
+  ul_El.textContent = ''
+  showElement();
+  console.log(listData)
+});
+
+
+
+
+
+
